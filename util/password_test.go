a15a7ccafd8a33d80347c85b7bb6bfc4640d1891
pepno <yizheliu035@gmail.com>
2023-07-15 19:00:20 +0800

    make password hashed

---
+++ util/password_test.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"github.com/stretchr/testify/require"
+	"golang.org/x/crypto/bcrypt"
+	"testing"
+)
+
+func TestPassword(t *testing.T) {
+	password := RandomString(6)
+	hashPassword, err := HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashPassword)
+
+	err = CheckPassword(password, hashPassword)
+	require.NoError(t, err)
+
+	wrongPassword := RandomString(6)
+	err = CheckPassword(wrongPassword, hashPassword)
+	require.EqualError(t, err, bcrypt.ErrMismatchedHashAndPassword.Error())
+}
