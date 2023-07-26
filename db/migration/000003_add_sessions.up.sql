fa7aa891565f16931d67cae30945bd31859e32f2
pepno <yizheliu035@gmail.com>
2023-07-26 19:49:24 +0800

    remove

--- db/migration/000003_add_sessions.up.sql
+++ db/migration/000003_add_sessions.up.sql
@@ -12,18 +12,3 @@ CREATE TABLE "sessions"
 
 ALTER TABLE "sessions"
     ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
-
-CREATE TABLE "sessions"
-(
-    "id"            uuid PRIMARY KEY,
-    "username"      varchar     NOT NULL,
-    "refresh_token" varchar     NOT NULL,
-    "user_agent"    varchar     NOT NULL,
-    "client_ip"     varchar     NOT NULL,
-    "is_blocked"    boolean     NOT NULL DEFAULT false,
-    "expires_at"    timestamptz NOT NULL,
-    "created_at"    timestamptz NOT NULL DEFAULT (now())
-);
-
-ALTER TABLE "sessions"
-    ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
