24679257900a71a82aff1d005e174adf16b16216
pepno <yizheliu035@gmail.com>
2023-08-16 23:21:25 +0800

    update ci yml

--- main.go
+++ main.go
@@ -107,7 +107,7 @@ func runGatewayServer(config util.Config, store db.Store) {
 	//open swagger when start
 	go func() {
 		addr := "http://localhost:8080/swagger"
-		err := open.Run(addr)
+		err = open.Run(addr)
 		if err != nil {
 			log.Fatal("can not open swagger api page ", err)
 		}
